787e8967dcf5622b92bb57a2e1c1f7ac375d66fa
Nathan Taylor <nathantaylor49@gmail.com>
2020-07-30 14:44:55 +0300

    Update index.ts

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,15 +1,5 @@
-import * as config from "./config";
-import * as database from "./database";
-import * as factories from "./factories";
-import * as logger from "./logger";
-import * as sms from "./sms";
-
-export default () => {
-  return {
-    ...sms,
-    ...database,
-    ...factories,
-    ...config,
-    ...logger,
-  };
-};
+export * from "./config";
+export * from "./database";
+export * from "./factories";
+export * from "./logger";
+export * from "./sms";
